cb3b4508fc5d39b0bc7f57bc1a7510d964db26c0
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-07-24 20:44:09 -0300

    feat: add zustand for state management and create user store

---
+++ src/store/userStore.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface UserStore {
+  user: { id: string; name: string; email: string } | null;
+  setUser: (user: UserStore["user"]) => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}));
